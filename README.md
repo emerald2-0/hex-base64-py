46d3dc4f4626226d5d219b732c5fb5f3b7116744
emerald2-0 <46712978+emerald2-0@users.noreply.github.com>
2020-08-28 01:24:46 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 Just copy the hexas and press Enter!
 That simple for now. Noobying:-)
+
+- Settled the program to a function to enable future developments 
+
+- Was to add ceasar cypher later.
--- b64_encoder.py
+++ b64_encoder.py
@@ -1,6 +1,45 @@
 import codecs
+import time
 
-hexa_input = input("Copy the hex code here: ")
-b64_result = codecs.encode(codecs.decode(hexa_input, 'hex'), 'base64').decode()
+def hex_to_base64():
+	hexa_input = input("Enter the hex code here: ")
+	#Remove the spaces in the input:
+	""""
+	if hexa_input.isspace() == True:
+		hexa_input.replace(' ', '')
+		time.sleep(0.4)
+		print("Converting to base64...")
+		time.sleep(0.4)
+		b64_result = codecs.encode(codecs.decode(hexa_input, 'hex'), 'base64').decode()
+		print("Conversion complete...")
 
-print(b64_result)
+	if hexa_input.isspace() != True:
+		time.sleep(0.4)
+		print("Converting to base64...")
+		time.sleep(0.4)
+		b64_result = codecs.encode(codecs.decode(hexa_input, 'hex'), 'base64').decode()
+		print("Conversion complete...")
+	"""
+	time.sleep(0.4)
+	print("Converting to base64...")
+	time.sleep(0.4)
+	b64_result = codecs.encode(codecs.decode(hexa_input, 'hex'), 'base64').decode()
+	print("Conversion complete...")
+
+	return b64_result
+
+print("Cyphers supported: ")
+time.sleep(0.4)
+print("1. Hexa to Base64 ")
+print("2. Ceaser Cypher ")
+cypher_method = int(input("Select the a cypher method: "))
+
+if cypher_method == 1:
+	print("../")
+	time.sleep(0.7)
+	print("Refering to hexa conversion...")
+	time.sleep(0.7)
+	hex_to_base64()
+
+if cypher_method != 1:
+	print("No other method supported for now..")
