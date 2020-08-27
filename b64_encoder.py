2a28a2c75353eee92f44606ed429ee400e6b88e5
emerald2-0 <46712978+emerald2-0@users.noreply.github.com>
2020-08-28 01:26:33 +0300

    Update b64_encoder.py

--- b64_encoder.py
+++ b64_encoder.py
@@ -3,29 +3,12 @@ import time
 
 def hex_to_base64():
 	hexa_input = input("Enter the hex code here: ")
-	#Remove the spaces in the input:
-	""""
-	if hexa_input.isspace() == True:
-		hexa_input.replace(' ', '')
-		time.sleep(0.4)
-		print("Converting to base64...")
-		time.sleep(0.4)
-		b64_result = codecs.encode(codecs.decode(hexa_input, 'hex'), 'base64').decode()
-		print("Conversion complete...")
-
-	if hexa_input.isspace() != True:
-		time.sleep(0.4)
-		print("Converting to base64...")
-		time.sleep(0.4)
-		b64_result = codecs.encode(codecs.decode(hexa_input, 'hex'), 'base64').decode()
-		print("Conversion complete...")
-	"""
 	time.sleep(0.4)
 	print("Converting to base64...")
 	time.sleep(0.4)
 	b64_result = codecs.encode(codecs.decode(hexa_input, 'hex'), 'base64').decode()
 	print("Conversion complete...")
-
+	
 	return b64_result
 
 print("Cyphers supported: ")
@@ -35,10 +18,8 @@ print("2. Ceaser Cypher ")
 cypher_method = int(input("Select the a cypher method: "))
 
 if cypher_method == 1:
-	print("../")
-	time.sleep(0.7)
 	print("Refering to hexa conversion...")
-	time.sleep(0.7)
+	time.sleep(0.4)
 	hex_to_base64()
 
 if cypher_method != 1:
